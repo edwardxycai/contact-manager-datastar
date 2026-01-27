ea65e86c5204af245c7008c692d0834abc6e6c8e
edwardxycai <edward.xy.cai@gmail.com>
2026-01-27 11:52:35 +0800

    update package.json, jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,14 @@
 export default {
   testEnvironment: "node",
+
+  // Only run backend/API tests
+  testMatch: [
+    '**/tests/**/*.test.js'
+  ],
+
+  // Explicitly ignore Playwright E2E tests
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/tests/e2e/'
+  ],  
 };
